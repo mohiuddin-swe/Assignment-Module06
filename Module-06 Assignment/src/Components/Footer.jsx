2dc2dd18b7bf91606268800ffe5f8452f4d77a93
Mohiuddin <101384674+mohiuddin-swe@users.noreply.github.com>
2023-07-11 23:15:12 +0600

    Delete Module-06 Assignment directory

--- Module-06 Assignment/src/Components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-      <footer className="bg-gray-800 text-white p-4 text-center ">
-        Created by Md Mohi Uddin
-      </footer>
-    );
-  }
-
-  export default Footer;
--- Module-06 Assignment/src/Components/Header.jsx
+++
@@ -1,5 +0,0 @@
-function Header({ title }) {
-    return <h1 className="text-4xl font-bold text-center my-8 bg-gray-800 text-white p-4 text-center">{title}</h1>;
-  }
-
-  export default Header;
